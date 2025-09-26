415a1e7c230a1f83b90623a8c9056596489c953c
sshen070 <SujithShen@gmail.com>
2025-09-25 21:41:30 -0700

    Accepts a file name & page number

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
 import sys
 from pathlib import Path
-import fitz
-# from line_classifier import parse_pdf
+from line_classifier import parse_pdf
 
 
-def main(arg_file1: str) -> None:
+def main(arg_file1: str, page_number: int) -> None:
 
     main_dir = Path(__file__).parent.parent
     print(f"Source Directory: {main_dir} \n")
 
     input_path = main_dir / "files" / arg_file1
 
-    print(f"Input Path: {input_path}")
+    # print(f"Input Path: {input_path}")
 
-    # # parse_pdf(input_path)
+    parsed = parse_pdf(input_path, page_number)
 
-    doc = fitz.open(input_path)
-    print(f"Pages: {doc.page_count}")
+    for item in parsed:
+        print(item)
 
 
-
+    
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <pdf_file_name> <page_number>")
+        sys.exit(1)
+
+    main(sys.argv[1], int(sys.argv[2]))
 
 
 
