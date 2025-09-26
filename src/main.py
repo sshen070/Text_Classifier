872d1beae29d3c014b24a6c3740d300b40219fb6
sshen070 <SujithShen@gmail.com>
2025-09-25 17:29:37 -0700

    Modified main.py to print out # of pages in pdf

--- src/main.py
+++ src/main.py
@@ -6,16 +6,16 @@ import fitz
 
 def main(arg_file1: str) -> None:
 
-    main_dir = Path(__file__).resolve().parent.parent
-    print(f"Source Directory: {main_dir}")
+    main_dir = Path(__file__).parent.parent
+    print(f"Source Directory: {main_dir} \n")
 
     input_path = main_dir / "files" / arg_file1
 
     print(f"Input Path: {input_path}")
 
-    # parse_pdf(input_path)
+    # # parse_pdf(input_path)
 
-    doc = fitz.open("files/CFR-2024-title21-vol8-chapI-subchapH.pdf")
+    doc = fitz.open(input_path)
     print(f"Pages: {doc.page_count}")
 
 
