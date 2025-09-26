3bc1af47ddd9e6907b5c7be99c215bc052c5b0ef
sshen070 <SujithShen@gmail.com>
2025-09-25 21:40:01 -0700

    Algorithm sucessfully parses through page and returns a dict containing line numbers & respective text

--- src/line_classifier.py
+++ src/line_classifier.py
@@ -1,18 +1,27 @@
-# # import fitz
+import fitz
 
-# # def parse_pdf(pdf_path: str) -> list[dict]:
-# #     results = []
-# #     doc = fitz.open(pdf_path)
+def parse_pdf(pdf_path: str, page_number: int) -> list[dict]:
 
-# #     for page_num, page in enumerate(doc, start=1):
-# #         text = page.get_text("text")
-# #         lines = text.splitlines()
-# #         for line in lines:
-# #             if line.strip():
-# #                 results.append({
-# #                     "page": page_num,
-# #                     "text": line.strip(),
-# #                 })
-# #     return results
+    results = []
+    doc = fitz.open(pdf_path)
 
+    if (page_number < 1 or page_number > doc.page_count):
+        print(f"Invalid page number: {page_number}. Document has {doc.page_count} pages.")
+        return results
 
+    # Holds the page object itself --> location
+    page = doc.load_page(page_number - 1)
+
+    # Extract text from the page --> raw text (str) --> lines (list)
+    text: str = page.get_text("text")
+    lines: list[str] = text.splitlines()
+        
+
+    for line_number, line in enumerate(lines, start=0):
+        if line.strip():
+            results.append({
+                "Line": line_number,
+                "Text": line
+            })
+
+    return results
